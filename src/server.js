d3edd50f02e679c3404c421e932cbb3c3fe79995
stpn94 <stpn94@gmail.com>
2021-11-02 16:27:11 +0900

    good

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-/* SOCKET.IO 사용 */
 import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
@@ -15,11 +14,20 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done();
-    }, 10000);
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", msg);
+    done();
   });
 });
 
