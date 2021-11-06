13f676f24d0146e2ccaab2f5dc49a53d68ae8614
stpn94 <stpn94@gmail.com>
2021-11-06 22:48:00 +0900

    ??

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,55 +1,259 @@
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
-room.hidden = true;
+const call = document.getElementById("call");
 
+// call : 카메라 사용
+call.hidden = true;
+
+// stream은 비디오와 오디오가 결합된것
+// stream의 멋진점 track이라는 것을 제공 (비디오 track, 오디오 track, 자막 track)하고 접근도 가능
+let myStream;
+let muted = false;
+let cameraOff = false;
 let roomName;
+let myPeerConnection;
+let myDataChannel;
 
-function addMessage(message) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message;
-  ul.appendChild(li);
+// async : 비동기 키워드
+// async function()은 await 키워드가 비동기 코드를 호출할 수 있게 해주는 함수
+// 이제 코드가 Promise를 반환
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function handleMessageSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector("input");
-  const value = input.value;
-  socket.emit("new_message", input.value, roomName, () => {
-    addMessage(`You: ${value}`);
-  });
-  input.value = "";
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } }, // 꼭 내가 지정한 비디오를 사용할때 exact 사용
+  };
+
+  // devoceId로 여기서 새로운 스트림을 만든다.
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function showRoom() {
+// getMedia();
+
+function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+function handleCameraClick() {
+  console.log(myStream.getVideoTracks());
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+
+// getMedia fucntion에 사용하려는 특정 카메라 id를 전송하여 새로운 스트림을 생성
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+  // 선택한 새 장치로 업데이트 된 video track을 받는다.
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
+}
+// Sender는 peer로 보내진 media stream track을 컨트롤하게 해준다.
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange); // 두개 이상 있어야 감지
+
+// webRTC web Real-Time Communication : 실시간 커뮤니케이션을 가능하게 해주는 기술, peer-to-peer 연결 가능
+// 이때까지 chat한 방식 : A가 B에게 메시지를 서버에 보내는 경우 A는 서버에게 메시지를 보내고 서버는 A가 보낸 메시지를 B에게 보내준다. (서버에 비용이 많이든다.)
+// peer-to-peer : A가 B에게 메시지를 보내는 경우 A의 영상과 오디오 텍스트가 서버로 가지 않는다. 즉 직접 B로 간다. (signaling을 하기위해 서버가 필요하긴(브라우저로 하여금 서버는 상대가 어디에 있는지 알게 하는 용도) 하지만 영상이나 오디오를 전송하기 위해 필요하진않다.)
+
+// welcome Form (join a room)
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
   welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName}`;
-  const form = room.querySelector("form");
-  form.addEventListener("submit", handleMessageSubmit);
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
 }
 
-function handleRoomSubmit(event) {
+async function handleWelcomeSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
+  const input = welcomeForm.querySelector("input");
+  // web Socket들의 속도가 media를 가져오는 속도나 연결을 만드는 속도보다 빠르다.
+  // 그래서 getMedia 하고 makeConnection을 한다음에 이벤트를 emit해야 한다.
+  await initCall();
+  socket.emit("join_room", input.value);
   roomName = input.value;
   input.value = "";
 }
 
-form.addEventListener("submit", handleRoomSubmit);
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
-socket.on("welcome", () => {
-  addMessage("someone joined!");
+// Socket Code
+
+// Peer A에서 돌아가는 코드
+// 1. Peer A에서 offer를 만들고 setLocalDescription하고 Peer B로 offer를 보낸다.
+socket.on("welcome", async () => {
+  myDataChannel = myPeerConnection.createDataChannel("chat");
+  myDataChannel.addEventListener("message", showChat);
+  console.log("made data channel");
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent offer");
+  socket.emit("offer", offer, roomName);
 });
 
-socket.on("bye", () => {
-  addMessage("someone left ㅠㅠ");
+// 다른 브라우저인 Peer B에서 돌아가는 코드
+// 3.Peer A에서 보낸 offer를 Peer B가 받아서 remoteDescription을 설정 후 answer를 보냄
+socket.on("offer", async (offer) => {
+  myPeerConnection.addEventListener("datachannel", (event) => {
+    myDataChannel = event.channel;
+    myDataChannel.addEventListener("message", showChat);
+  });
+  console.log("received the offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
 });
 
-socket.on("new_message", addMessage);
+// 5.Peer B에서 보낸 answer로 Peer A에서 remoteDescription을 가지게 되었다.
+socket.on("answer", (answer) => {
+  console.log("received the offer");
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+  console.log("received candidate");
+  myPeerConnection.addIceCandidate(ice);
+});
+
+// RTC Code
+
+function makeConnection() {
+  // Peer to Peer 연결 만들고
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+
+  // track들을 개별적으로 추가해주는 함수
+  // 양쪽 브라우저에서 카메라, 마이크 데이터 stream을 받아서 그것들을 연결 후 안에 집어 넣었다.
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+
+  // 카메라를 바꾸는게 적용 되지 않아서 여기서 서로다른 ID로 새로운 stream을 만든다.
+}
+
+// offer와 answer을 가지고, 그걸 받는걸 모두 끝냈을 때
+// peer-to-peer 연결의 양쪽에서 icecandidate라는 이벤트를 실행하기 시작한다.
+// icecandidate(Internet Connectivity Establishment(인터넷 연결 생성)candidate) 뜻?
+// IceCandidate는 webRTC에 필요한 프로토콜을 의미하는데 멀리 떨어진 장치와 소통할 수 있게 하기 위함이다. -> 브라우저가 서로 소통할 수 있게 해주는 방법
+// 어떤 소통방법이 가장 좋을 것인지 제안할 때 사용
+// 다수의 candidate(후보)들이 각각의 연결에서 제안되고 그들은 서로의 동의 하에 하나를 선택한다. 그리고 그것을 소통 방식에 사용한다.
+// 그리고 candidate들을 다시 다른 브라우저로 보낸다.(왜냐 소통하는 방법들인 이 candidate들은 각자의 브라우저들에 의해 만들어지지만 다른 브라우저로 전송되지는 않으므로)
+// Peer A , Peer B 서로서로 보내야 한다.
+
+// ice를 받으면 그 icecandidate를 우리 서버로 보내겠다는 뜻
+// Peer A와 Peer B 브라우저가 candidate들을 서로 주고 받는다는 뜻
+function handleIce(data) {
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.stream;
+}
+
+// 채팅용
+const chatContainer = document.getElementById("chatContainer");
+const chatForm = document.getElementById("chatForm");
+
+function addMessage(message) {
+  const ul = chatContainer.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  li.style.listStyle = "none";
+  ul.appendChild(li);
+}
+
+chatForm.addEventListener("submit", handleChatForm);
+
+function handleChatForm(event) {
+  event.preventDefault();
+  const input = chatForm.querySelector("input");
+  const message = input.value;
+  addMessage(`You : ${message}`);
+  input.value = "";
+  myDataChannel.send(message);
+}
+
+function showChat(message) {
+  // const parsed = JSON.parse(message.data)
+  addMessage(`Other : ${message.data}`);
+}
--- src/server.js
+++ src/server.js
@@ -1,56 +1,41 @@
 import http from "http";
-import SocketIO from "socket.io";
 import express from "express";
+import SocketIO from "socket.io";
 
 const app = express();
-
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-const httpServer = http.createServer(app);
+const httpServer = http.createServer(app); // 먼저 내 http서버에 access /server-> httpServer 이름만 바꿈
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-  socket.on("enter_room", (roomName, done) => {
+  socket.on("join_room", (roomName) => {
     socket.join(roomName);
-    done();
     socket.to(roomName).emit("welcome");
   });
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+
+  // 2.Peer A에서 보낸 offer를 받음
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
   });
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", msg);
-    done();
+
+  // 4.answer를 받으면 방에 있는 모든 사람에게 알려야 한다.
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
   });
 });
 
-/*
-const wss = new WebSocket.Server({ server });
-const sockets = [];
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  console.log("Connected to Browser ✅");
-  socket.on("close", onSocketClose);
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
-  });
-}); */
-
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
+
+// 컴퓨터랑 폰이 같은 wifi에 있지 않으면 에러 생긴다.
+// 그래서 STUN 서버를 사용한다.
+// STUN 서버는 컴퓨터가 공용 IP주소를 찾게 해준다.
